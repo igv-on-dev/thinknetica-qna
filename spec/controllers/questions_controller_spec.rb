26d6d7a1741cbf96a901d337db5aa329d991f898
igv <igv.versn@gmail.com>
2016-03-23 10:09:45 +0300

    Added specs and implementations for QuestionsController#update

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -76,4 +76,39 @@ RSpec.describe QuestionsController, type: :controller do
       end
     end
   end
+
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      it 'assigns the requested question to @question' do
+        patch :update, id: question, question: attributes_for(:question)
+        expect(assigns(:question)).to eq question
+      end
+
+      it 'changes question attributes' do
+        patch :update, id: question, question: { title: 'new title', body: 'new body' }
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+
+      it 'redirects to the updated question' do
+        patch :update, id: question, question: { title: 'new title', body: 'new body' }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      before { patch :update, id: question, question: { title: 'new title', body: nil } }
+
+      it 'does not change question attributes' do
+        question.reload
+        expect(question.title).to eq attributes_for(:question)[:title]
+        expect(question.body).to eq attributes_for(:question)[:body]
+      end
+
+      it 're-render edit view' do
+        expect(response).to render_template :edit
+      end
+    end
+  end
 end
