03d064ddc5c70ee6c84af41464d55fadd18de2fc
igv <igv.versn@gmail.com>
2016-03-23 11:26:57 +0300

    Added specs and implementations for AnswersController#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :questions do
-    resources :answers, only: :new
+    resources :answers, only: [:new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect{ post :create, question_id: question, answer: attributes_for(:answer) }
+            .to change(Answer, :count).by(1)
+      end
+
+      it 'adds answer to right question' do
+        expect{ post :create, question_id: question, answer: attributes_for(:answer) }
+            .to change(question.answers, :count).by(1)
+      end
+
+      it 'redirects to question view' do
+        expect(post :create, question_id: question, answer: attributes_for(:answer))
+            .to redirect_to question
+      end
+    end
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect{ post :create, question_id: question, answer: attributes_for(:invalid_answer) }
+            .to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question_id: question, answer: attributes_for(:invalid_answer)
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
