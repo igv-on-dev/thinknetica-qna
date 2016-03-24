0c581e0c766d95ec49f9bd449740a98eb11c7b4c
igv <igv.versn@gmail.com>
2016-03-24 10:37:38 +0300

    Removed unnecessary test

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -17,12 +17,7 @@ RSpec.describe AnswersController, type: :controller do
 
   describe 'POST #create' do
     context 'with valid attributes' do
-      it 'saves the new answer in the database' do
-        expect{ post :create, question_id: question, answer: attributes_for(:answer) }
-            .to change(Answer, :count).by(1)
-      end
-
-      it 'adds answer to right question' do
+      it 'saves the new answer in the database and link it with right question' do
         expect{ post :create, question_id: question, answer: attributes_for(:answer) }
             .to change(question.answers, :count).by(1)
       end
