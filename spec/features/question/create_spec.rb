d3e28a26d085c187830c60079e2866fec462ed2f
igv <igv.versn@gmail.com>
2016-03-30 00:53:10 +0300

    Implemented story "Only authenticated user is able to create a question"

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Create question', %q{
+  In order to get answer from community
+  As an authenticated user
+  I want to be able to ask questions
+} do
+  given(:user) { create(:user) }
+
+  scenario 'Authenticated user tries to create question' do
+    sign_in(user)
+
+    visit questions_path
+    click_on I18n.t('questions.index.ask_question')
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'Body of test question'
+    click_on I18n.t('questions.new.create')
+
+    expect(page).to have_content I18n.t('question.created')
+    expect(page).to have_content 'Test question'
+    expect(page).to have_content 'Body of test question'
+  end
+
+
+  scenario 'Non-authenticated user tries to create question' do
+    visit questions_path
+    click_on I18n.t('questions.index.ask_question')
+
+    expect(page).to have_content I18n.t('devise.failure.unauthenticated')
+  end
+end
