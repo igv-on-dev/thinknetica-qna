c7bb5118fb1dfccb48e106e3e4d7b10bbc59cbce
igv <igv.versn@gmail.com>
2016-04-17 05:30:15 +0300

    Story "User is able to see list of questions"

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,9 +2,15 @@ FactoryGirl.define do
   factory :answer do
     body 'MyText'
     question nil
-  end
 
-  factory :invalid_answer, class: Answer do
-    body nil
+    trait :without_body do
+      body nil
+    end
+
+    trait :with_sequential_body do
+      sequence(:body) { |n| "Body of answer #{n}" }
+    end
+
+    factory :invalid_answer, traits: [:without_body]
   end
 end
